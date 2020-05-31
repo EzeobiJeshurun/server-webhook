56583e132c416468fcfbec0c3e79383092ded819
EzeobiJeshurun <jeshurunnonso@gmail.com>
2020-06-01 00:30:23 +0100

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const socketio = require('socket.io');
+const cors = require('cors');
 const PORT = process.env.PORT||5000;
 const {getUserAndRoom,  incommingMessage, removeUser} = require('./messages');
 const http = require('http');
@@ -138,6 +139,7 @@ io.on('connection',(socket)=>{
 })
 
 app.use(router);
+app.use(cors());
 server.listen(PORT,()=>{
     console.log(`server has started on port ${PORT}`);
 });
